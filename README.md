83e37e2b7b9f00e5b3d8e81615bab9689e12d3ec
Mathieu Dumoulin <crazyone@crazycoders.net>
2015-10-02 13:54:48 -0400

    Added PreviousCurrentIterator Added readme Added tests Added travis support

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# compare-iterator
-Provides access to a very simple compare-iterator
+# Previous/Current iterator
+Allows iteration over an array but returns two items at a time stepping by one item at a time. Thus, the iterator for
+
+```PHP
+['a', 'b', 'c', 'd']
+```
+    
+Will return
+
+```PHP
+['previous' => 'a', 'current' => 'b']
+['previous' => 'b', 'current' => 'c']
+['previous' => 'c', 'current' => 'd']
+```
+
+# Installation
+
+To install it, just include this requirement into your composer.json
+
+```JSON
+{
+    "require": {
+        "crazycodr/previous-current-iterator": "1.*"
+    }
+}
+```
+And then run composer install/update as necessary.
+
+# Examples
+
+To use the PreviousCurrentIterator, just instanciate a copy with an array and foreach it!
+
+```PHP
+$data = ['a', 'b', 'c', 'd'];
+foreach(new PreviousCurrentIterator($data) as $keys => $values) {
+    //Compare previous and current
+    if ($values['previous'] !== $values['current']) {
+        echo 'Not the same<br />';
+    }
+}
+```
+
+# Use cases
+
+Practical if you need to compare two items together in a previous vs current or current vs next manner.
---
+++ src/PreviousCurrentIterator.php
@@ -0,0 +1,97 @@
+<?php
+namespace CrazyCodr\Iterators;
+
+/**
+ * DoubleSequenceIterator returns the previous and current item as it iterators, perfect to compare
+ * items that are stringed in a sequence such as historical or sequenced elements to compare to each other.
+ *
+ * @package CrazyCodr\Iterators
+ */
+class PreviousCurrentIterator implements \Iterator
+{
+
+	/**
+	 * Contains the items to iterate over
+	 *
+	 * @var array
+	 */
+	private $items = [];
+
+	/**
+	 * Contains the previous items (previous current) to compare with the real current
+	 * @var mixed
+	 */
+	private $previousItem = null;
+
+	/**
+	 * Contains the previous key to compare with the current key
+	 * @var null
+	 */
+	private $previousKey = null;
+
+	/**
+	 * Contains the previous items (previous current) to compare with the real current
+	 * @var mixed
+	 */
+	private $currentItem = null;
+
+	/**
+	 * Contains the previous key to compare with the current key
+	 * @var null
+	 */
+	private $currentKey = null;
+
+	/**
+	 * @param array $data
+	 */
+	public function __construct(array $data) {
+		$this->items = $data;
+	}
+
+	/**
+	 * @inheritedDoc
+	 */
+	public function current() {
+		return [
+			'previous' => $this->previousItem,
+			'current'  => $this->currentItem
+		];
+	}
+
+	/**
+	 * @inheritedDoc
+	 */
+	public function next() {
+		$this->previousItem = current($this->items);
+		$this->previousKey = key($this->items);
+		next($this->items);
+		$this->currentItem = current($this->items);
+		$this->currentKey = key($this->items);
+	}
+
+	/**
+	 * @inheritedDoc
+	 */
+	public function key() {
+		return [
+			'previous' => $this->previousKey,
+			'current'  => $this->currentKey
+		];
+	}
+
+	/**
+	 * @inheritedDoc
+	 */
+	public function valid() {
+		return $this->previousKey !== null && $this->currentKey !== null;
+	}
+
+	/**
+	 * @inheritedDoc
+	 */
+	public function rewind() {
+		reset($this->items);
+		$this->next();
+	}
+
+}
